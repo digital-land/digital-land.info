b55d4d601740539dc8131bd8551265375f08028c
Adam Shimali <adam.shimali@gmail.com>
2021-11-15 10:37:24 +0000

    Change load balancer port 80 listener to redirect to port 443 rather thank forward to load balancer.

--- tf/app/main.tf
+++ tf/app/main.tf
@@ -288,8 +288,13 @@ resource "aws_lb_listener" "dl_web_lb_listener" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.dl_web_lb_target_group.arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
