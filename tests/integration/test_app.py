f870079fe45bedf3ea910861efa6b1540b33137c
Toby Petty <tobypetty1@gmail.com>
2021-11-29 16:54:25 +0000

    #1355 Wrote initial failing test to test CORS functionality

---
+++ tests/integration/test_app.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from dl_web.factory import create_app
+
+app = create_app()
+client = TestClient(app)
+
+
+def test_response_cors_headers():
+    """
+    Test that the appropriate CORS headers are included in a simple GET request to server root
+    """
+    response = client.get('/', headers={'Origin': 'localhost'})
+    assert "Access-Control-Allow-Origin" in response.headers.keys()
+    assert response.headers["Access-Control-Allow-Origin"] == "*"
