262025e6102fb06592c423945a8f19bdb5c4d021
Adam Shimali <adam.shimali@gmail.com>
2022-01-18 12:57:13 +0000

    Split up some of the longer tests

--- tests/integration/test_entity_search.py
+++ tests/integration/test_entity_search.py
@@ -50,12 +50,15 @@ def params(raw_params):
     return raw_params.copy()
 
 
-def test_search_entity_by_dataset_name(test_data, params):
+def test_search_entity_by_dataset_name_not_in_system(test_data, params):
 
     params["dataset"] = ["not-exists"]
     result = get_entity_search(params)
     assert 0 == result["count_all"]
     assert [] == result["entities"]
+
+
+def test_search_entity_by_single_dataset_name(test_data, params):
 
     params["dataset"] = ["greenspace"]
     result = get_entity_search(params)
@@ -63,6 +66,9 @@ def test_search_entity_by_dataset_name(test_data, params):
     entity = result["entities"][0]
     assert "greenspace" == entity.dataset
     assert "geography" == entity.typology
+
+
+def test_search_entity_by_list_of_dataset_names(test_data, params):
 
     params["dataset"] = ["greenspace", "brownfield-site"]
     result = get_entity_search(params)
@@ -184,11 +190,10 @@ def test_search_entity_by_point(test_data, params):
     assert "historical-monument" == entity.dataset
 
 
-def test_search_entity_by_polygon_intersects(test_data, params):
+def test_search_entity_by_single_polygon_intersects(test_data, params):
 
     from tests.test_data.wkt_data import intersects_with_brownfield_entity as brownfield
     from tests.test_data.wkt_data import intersects_with_greenspace_entity as greenspace
-    from tests.test_data.wkt_data import no_intersection
 
     params["geometry"] = [brownfield]
     params["geometry_relation"] = GeometryRelation.intersects.name
@@ -204,23 +209,49 @@ def test_search_entity_by_polygon_intersects(test_data, params):
     entity = result["entities"][0]
     assert "greenspace" == entity.dataset
 
+
+def test_search_entity_by_list_of_polygons_that_intersect(test_data, params):
+
+    from tests.test_data.wkt_data import intersects_with_brownfield_entity as brownfield
+    from tests.test_data.wkt_data import intersects_with_greenspace_entity as greenspace
+
     params["geometry"] = [brownfield, greenspace]
+    params["geometry_relation"] = GeometryRelation.intersects.name
+
     result = get_entity_search(params)
     assert 2 == result["count_all"]
 
+
+def test_search_entity_by_polygon_with_no_intersection(test_data, params):
+
+    from tests.test_data.wkt_data import no_intersection
+
     params["geometry"] = [no_intersection]
+    params["geometry_relation"] = GeometryRelation.intersects.name
+
     result = get_entity_search(params)
     assert 0 == result["count_all"]
 
 
-def test_search_entity_by_entries_option(test_data, params):
+def test_search_all_entities(test_data, params):
 
     # default is EntriesOption.all - already in params
     result = get_entity_search(params)
     assert 4 == result["count_all"]
+    for e in result["entities"]:
+        assert e.dataset in [
+            "greenspace",
+            "forest",
+            "brownfield-site",
+            "historical-monument",
+        ]
+
+
+def test_search_current_entries(test_data, params):
 
     # entries without an end date
     params["entries"] = EntriesOption.current
+
     result = get_entity_search(params)
     assert 3 == result["count_all"]
     for e in result["entities"]:
@@ -230,8 +261,12 @@ def test_search_entity_by_entries_option(test_data, params):
             "historical-monument",
         ]
 
+
+def test_search_historical_entries(test_data, params):
+
     # entries with an end date
     params["entries"] = EntriesOption.historical
+
     result = get_entity_search(params)
     assert 1 == result["count_all"]
     e = result["entities"][0]
