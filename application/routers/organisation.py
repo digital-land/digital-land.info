4a41ce7e4bcfaf275f9a49c012576ffb1d74a3fa
Adam Shimali <adam.shimali@gmail.com>
2022-05-26 16:04:05 +0100

    Start html view of organisations

--- application/routers/organisation.py
+++ application/routers/organisation.py
@@ -1,11 +1,12 @@
 import logging
 from typing import Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from sqlalchemy import func
 from starlette.responses import HTMLResponse
 
 from application.core.models import OrganisationModel, OrganisationsByTypeModel
+from application.core.templates import templates
 from application.core.utils import DigitalLandJSONResponse
 from application.db.models import OrganisationOrm
 from application.db.session import get_context_session
@@ -16,6 +17,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_organisations(
+    request: Request,
     extension: Optional[SuffixOrganisation] = None,
 ) -> OrganisationsByTypeModel:
     with get_context_session() as session:
@@ -33,7 +35,10 @@ def get_organisations(
     if extension == SuffixOrganisation.json:
         return OrganisationsByTypeModel(organisations=organisations_by_type)
     else:
-        return "OK"
+        return templates.TemplateResponse(
+            "organisation_index.html",
+            {"request": request, "organisations": organisations_by_type},
+        )
 
 
 router.add_api_route(
