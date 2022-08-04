5a345395f1698aea3b4d37527a502e9e81265a98
Adam Shimali <adam.shimali@gmail.com>
2022-08-04 11:25:43 +0100

    Updated some variables to snake case.

--- application/routers/guidance_.py
+++ application/routers/guidance_.py
@@ -10,37 +10,37 @@ logger = logging.getLogger(__name__)
 # get the 'page name' for use in tracking current page in
 # navigation
 def get_pagename(path):
-    splitPath = path.split("/")
-    pageName = splitPath[-1]
-    if len(splitPath) >= 2:
-        if splitPath[-1] == "index.html" or splitPath[-1] == "":
-            pageName = splitPath[-2]
-    if pageName == "" or pageName == "index.html":
-        pageName = "home"
-    return pageName.replace(".html", "")
+    split_path = path.split("/")
+    page_name = split_path[-1]
+    if len(split_path) >= 2:
+        if split_path[-1] == "index.html" or split_path[-1] == "":
+            page_name = split_path[-2]
+    if page_name == "" or page_name == "index.html":
+        page_name = "home"
+    return page_name.replace(".html", "")
 
 
 # using the url path construct a breadcrumb dictionary for use in the view
 def get_breadcrumbs(path):
-    splitPath = path.split("/")
-    splitPath.reverse()
-    totalItems = len(splitPath)
-    crumbDict = [{"href": "/guidance/", "text": "Guidance"}]
-    # loop for the number of times there are items in the splitPath list
-    for i in range(totalItems):
+    split_path = path.split("/")
+    split_path.reverse()
+    total_items = len(split_path)
+    crumb_dict = [{"href": "/guidance/", "text": "Guidance"}]
+    # loop for the number of times there are items in the split_path list
+    for i in range(total_items):
 
         # instantiate some variables
         index = i + 1
-        text = splitPath[-index]
-        href = splitPath[-index]
+        text = split_path[-index]
+        href = split_path[-index]
 
         if text == "index.html":
             if index == 1:
-                text = splitPath[-1]
-                href = splitPath[-1]
+                text = split_path[-1]
+                href = split_path[-1]
             if index >= 2:
-                text = splitPath[-2]
-                href = splitPath[i]
+                text = split_path[-2]
+                href = split_path[i]
 
         # if it is not an index page
         if text != "index.html":
@@ -48,59 +48,59 @@ def get_breadcrumbs(path):
                 # if to a subsection index page
                 # (because less that total items in list of split
                 # url which has been reversed)
-                if index < totalItems:
-                    href = "../" + splitPath[-1] + "/"
+                if index < total_items:
+                    href = f"../{split_path[-1]}/"
                 else:
-                    href = splitPath[-1]
+                    href = split_path[-1]
             if index >= 2:
-                href = splitPath[-2]
+                href = split_path[-2]
 
             # finally add the link if it is not an index page
-            crumbDict.append(
+            crumb_dict.append(
                 {
                     "text": text.replace(".html", "").replace("-", " "),
                     "href": href,
                 }
             )
 
-    return crumbDict
+    return crumb_dict
 
 
 @router.get("/{url_path:path}")
 async def catch_all(request: Request, url_path: str):
 
-    indexFile = "index.html"
+    index_file = "index.html"
 
     # if URL path in this route is empty
     if url_path == "":
-        url_path += indexFile
+        url_path += index_file
     # if URL path in this route ends with /
     elif url_path[-1] == "/":
-        url_path += indexFile
+        url_path += index_file
 
     # build string of the URL path and then the system path to the template file
-    rootURLPath = "pages/guidance/"
-    urlPathTofile = rootURLPath + url_path
-    sysPathToFile = "application/templates/" + urlPathTofile
+    root_url_path = "pages/guidance/"
+    url_path_to_file = root_url_path + url_path
+    sys_path_to_file = f"application/templates/{url_path_to_file}"
 
     # if matched path is to a directory assume looking for index file
-    if os.path.isdir(sysPathToFile):
-        urlPathTofile += indexFile
-        sysPathToFile += indexFile
+    if os.path.isdir(sys_path_to_file):
+        url_path_to_file += index_file
+        sys_path_to_file += index_file
 
     # if template file exists use it to render the page based
     # on the corresponding URL Path
-    if os.path.exists(sysPathToFile) and os.path.isfile(sysPathToFile):
+    if os.path.exists(sys_path_to_file) and os.path.isfile(sys_path_to_file):
 
         return templates.TemplateResponse(
-            urlPathTofile,  # path to the template file we wish to render
+            url_path_to_file,  # path to the template file we wish to render
             {
                 "request": request,
                 # a dictionary of useful data used by the tempaltes and layouts
                 "pageData": {
-                    "root_url": rootURLPath,
+                    "root_url": root_url_path,
                     "url_path": url_path,
-                    "url_path_page": urlPathTofile,
+                    "url_path_page": url_path_to_file,
                     "name": get_pagename(url_path),
                     "breadcrumbs": get_breadcrumbs(url_path),
                 },
