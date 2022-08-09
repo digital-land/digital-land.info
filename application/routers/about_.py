2bfacdee106d0f3e155d8d59cec0b49b44d34ec6
Paul Smith <mail@paulsmith.site>
2022-08-09 16:37:51 +0100

    Feature/about (#75)
    * updates with new about section
    
    * updates navigation text
    
    * update footer content

---
+++ application/routers/about_.py
@@ -0,0 +1,112 @@
+import os
+import logging
+from fastapi import APIRouter, Request
+from application.core.templates import templates
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+
+# get the 'page name' for use in tracking current page in
+# navigation
+def get_pagename(path):
+    split_path = path.split("/")
+    page_name = split_path[-1]
+    if len(split_path) >= 2:
+        if split_path[-1] == "index.html" or split_path[-1] == "":
+            page_name = split_path[-2]
+    if page_name == "" or page_name == "index.html":
+        page_name = "home"
+    return page_name.replace(".html", "")
+
+
+# using the url path construct a breadcrumb dictionary for use in the view
+def get_breadcrumbs(path):
+    split_path = path.split("/")
+    split_path.reverse()
+    total_items = len(split_path)
+    crumb_dict = [{"href": "/about/", "text": "About"}]
+    # loop for the number of times there are items in the split_path list
+    for i in range(total_items):
+
+        # instantiate some variables
+        index = i + 1
+        text = split_path[-index]
+        href = split_path[-index]
+
+        if text == "index.html":
+            if index == 1:
+                text = split_path[-1]
+                href = split_path[-1]
+            if index >= 2:
+                text = split_path[-2]
+                href = split_path[i]
+
+        # if it is not an index page
+        if text != "index.html":
+            if index == 1:
+                # if to a subsection index page
+                # (because less that total items in list of split
+                # url which has been reversed)
+                if index < total_items:
+                    href = f"../{split_path[-1]}/"
+                else:
+                    href = split_path[-1]
+            if index >= 2:
+                href = split_path[-2]
+
+            # finally add the link if it is not an index page
+            crumb_dict.append(
+                {
+                    "text": text.replace(".html", "").replace("-", " "),
+                    "href": href,
+                }
+            )
+
+    return crumb_dict
+
+
+@router.get("/{url_path:path}")
+async def catch_all(request: Request, url_path: str):
+
+    index_file = "index.html"
+
+    # if URL path in this route is empty
+    if url_path == "":
+        url_path += index_file
+    # if URL path in this route ends with /
+    elif url_path[-1] == "/":
+        url_path += index_file
+
+    # build string of the URL path and then the system path to the template file
+    root_url_path = "pages/about/"
+    url_path_to_file = root_url_path + url_path
+    sys_path_to_file = f"application/templates/{url_path_to_file}"
+
+    # if matched path is to a directory assume looking for index file
+    if os.path.isdir(sys_path_to_file):
+        url_path_to_file += index_file
+        sys_path_to_file += index_file
+
+    # if template file exists use it to render the page based
+    # on the corresponding URL Path
+    if os.path.exists(sys_path_to_file) and os.path.isfile(sys_path_to_file):
+
+        return templates.TemplateResponse(
+            url_path_to_file,  # path to the template file we wish to render
+            {
+                "request": request,
+                # a dictionary of useful data used by the tempaltes and layouts
+                "pageData": {
+                    "root_url": root_url_path,
+                    "url_path": url_path,
+                    "url_path_page": url_path_to_file,
+                    "name": get_pagename(url_path),
+                    "breadcrumbs": get_breadcrumbs(url_path),
+                },
+            },
+        )
+    else:
+        return templates.TemplateResponse(
+            "404.html", {"request": request}, status_code=404
+        )
---
+++ application/templates/pages/about/about.md
@@ -0,0 +1,44 @@
+Planning Data aims to make land and housing data easier to find, understand, use and trust.
+
+We believe this will lead to:
+
+-   better planning decisions
+-   fewer uncertainties and delays in the housing system
+-   more new digital services for property and planning
+-   better relationships between communities and developments
+
+Our platform collects planning and housing data from local planning authorities (LPAs) and transforms it into a consistent state, across England. Anyone can view, download and analyse the data we hold. We are currently working with a set number of LPAs to collect data.
+
+To find out how our platform is developing, please see our [roadmap](https://docs.google.com/document/d/1PmeB9EhqTiYu7azUgvVx_0b3m6cF6i3J3xphsTbBcI0/edit#heading=h.w8gd6srols80).
+
+Data collections
+----------------
+
+You can view the data we currently have as [text](https://www.digital-land.info/dataset/) or on a [map](https://www.digital-land.info/map/).
+
+LPAs store data in different ways. We're working with our partners on how to collect the data in a way that the platform understands, with minimum work and technical expertise for LPAs. This means ongoing work on the platform and data collection pipeline, as well as creating data standards.
+
+### Data standards
+
+Data standards are instructions for how to organise data, in order to share it effectively. They make it possible for platforms like Planning Data to collect data from a wide range of sources, and automatically produce correct and consistent outputs.
+
+For example, we need all LPAs to enter dates in the same way, so our platform knows that 1-2-2020 always means the 1st of February - not the 2nd of January.
+
+We want to minimise the work LPAs will need to do to contribute their data. We have created guidance on how to meet the standards, and will continue to revise this through user testing.
+
+How the data will be used
+-------------------------
+
+The data is openly available for anyone to download, analyse, interpret, and use to develop tools and services.
+
+There are 2 services already in development:
+
+-   [Back Office Planning System (BoPS)](https://bops.digital)
+-   [Reduce Invalid Planning Applications (RIPA)](https://www.ripa.digital/understanding-the-problem)
+
+Services like RIPA and BOPS have the potential to save LPAs millions each year, as well as improving the planning process for homeowners and developers.
+
+Feedback
+--------
+
+Please send any site feedback to <digitalland@levellingup.gov.uk>.
