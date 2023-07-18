04853229de6d7679c5463a720ed7f9844ee4d726
George Goodall <ggoodall@scottlogic.com>
2023-07-18 15:48:49 +0100

    improve the test test_find_an_entity_via_the_search_page

--- tests/acceptance/test_entity.py
+++ tests/acceptance/test_entity.py
@@ -9,6 +9,57 @@ from multiprocessing.context import Process
 
 from application.app import create_app  # noqa: E402
 
+from tests.utils.database import add_entities_to_database
+
+# define some test data
+mockEntities = [
+    {
+        "entity": "106",
+        "name": "A space",
+        "entry_date": "2019-01-07",
+        "start_date": "2019-01-05",
+        "end_date": "2020-01-07",
+        "dataset": "greenspace",
+        "json": None,
+        "organisation_entity": None,
+        "prefix": "greenspace",
+        "reference": "Q1234567",
+        "typology": "geography",
+        "geometry": "MultiPolygon (((-0.3386878967285156 53.74426323597749, -0.337904691696167 53.743857158459996, -0.33673524856567383 53.744003093019586, -0.33637046813964844 53.74463124033804, -0.3365743160247803 53.74525937826645, -0.33737897872924805 53.74541799747043, -0.33875226974487305 53.74505000000031, -0.3386878967285156 53.74426323597749)))",  # noqa: E501
+        "point": "POINT (-0.33737897872924805 53.74541799747043)",
+    },
+    {
+        "entity": "107",
+        "name": "B space",
+        "entry_date": "2019-01-07",
+        "start_date": None,
+        "end_date": "2020-01-07",
+        "dataset": "Brownfield site",
+        "json": None,
+        "organisation_entity": None,
+        "prefix": "greenspace",
+        "reference": "Q444444",
+        "typology": "geography",
+        "geometry": None,
+        "point": "POINT (-0.33737897872924805 53.74541799747043)",
+    },
+    {
+        "entity": "108",
+        "name": "C space",
+        "entry_date": "2019-01-07",
+        "start_date": None,
+        "end_date": "2020-01-07",
+        "dataset": "Forest",
+        "json": None,
+        "organisation_entity": None,
+        "prefix": "wikidata",
+        "reference": "Q20648596",
+        "typology": "organisation",
+        "geometry": None,
+        "point": None,
+    },
+]
+
 app = create_app()
 
 HOST = "0.0.0.0"
@@ -30,7 +81,9 @@ def server_process():
     proc.kill()
 
 
-def test_correctly_loads_the_entity_root(server_process, page):
+def test_correctly_loads_the_entity_root(
+    server_process, page, add_base_entities_to_database_yield_reset
+):
     page.goto(BASE_URL + ENTITY_ROUTE)
     assert page.title() == "Search for planning data"
     resultsText = page.locator(".app-results-summary__title").inner_text()
@@ -46,58 +99,108 @@ def test_correctly_loads_the_entity_root(server_process, page):
     assert mapControls.count() > 1
 
 
-def test_find_an_entity_via_the_search_page(server_process, page):
+def test_find_an_entity_via_the_search_page(server_process, page, empty_database):
+    add_entities_to_database(mockEntities)
+
     page.goto(BASE_URL)
     page.get_by_role("link", name="Search", exact=True).click()
 
+    # check if A space and B space are in the results
+    assert page.get_by_role("heading", name="A space") is not None
+    assert page.get_by_role("heading", name="B space") is not None
+    assert page.get_by_role("heading", name="C space") is not None
+
+    # check that only two results are returned
     resultsCountText = page.locator(
         "//h2[contains(@class, 'app-results-summary__title')]"
     ).first.text_content()
     numberOfResults = int("".join(filter(str.isdigit, resultsCountText)))
-    assert numberOfResults > 0
+    assert numberOfResults == mockEntities.__len__()
 
-    with page.expect_navigation() as navigation_info:
-        page.get_by_label("Brownfield site").check()
-        page.get_by_role("button", name="Search").click()
+    # check that A and B space has a map and C space doesn't
+    assert page.locator("[id='106-map']").count() == 1
+    assert page.locator("[id='107-map']").count() == 1
+    assert page.locator("[id='108-map']").count() == 0
 
-    response = navigation_info.value
-    assert response.status == 200
-
-    page.get_by_label("Forest").check()
-    page.get_by_label("Brownfield site").uncheck()
-    page.get_by_label("Day").click()
-    page.get_by_label("Day").fill("01")
-    page.get_by_label("Day").press("Tab")
-    page.get_by_label("Month").fill("01")
-    page.get_by_label("Month").press("Tab")
-    page.get_by_label("Year").fill("2010")
-    page.get_by_label("Since").check()
+    # filter down to just get geographies
+    page.get_by_role("heading", name="Typology").click()
+    page.get_by_label("geography").check()
     page.get_by_role("button", name="Search").click()
 
-    resultsCountText = page.locator(
-        "//h2[contains(@class, 'app-results-summary__title')]"
-    ).first.text_content()
-    numberOfResults = int("".join(filter(str.isdigit, resultsCountText)))
-    assert numberOfResults > 0
+    # check that only two results are returned
+    assert page.get_by_role("heading", name="A space").count() == 1
+    assert page.get_by_role("heading", name="B space").count() == 1
+    assert page.get_by_role("heading", name="C space").count() == 0
 
-    page.get_by_role("group").filter(
-        has_text="Entries1 selected Entries Which type of entries do you want to see? All Current "
-    ).get_by_role("img").click()
-    page.get_by_label("Current").check()
-    page.get_by_role("button", name="Search").click()
-    page.get_by_role("link", name="current").click()
-    page.get_by_role("link", name="current").click()
-    page.get_by_role("link", name="current").click()
-    page.get_by_role("link", name="current").click()
-    page.get_by_role("link", name="DateOption.since").click()
+    # filter down to just get A space
+    page.get_by_label("Greenspace").check()
     page.get_by_role("button", name="Search").click()
 
-    with page.expect_navigation() as navigation_info:
-        page.get_by_label("Brownfield site").check()
-        page.get_by_role("button", name="Search").click()
+    # check that only A space is returned
+    assert page.get_by_role("heading", name="A space").count() == 1
+    assert page.get_by_role("heading", name="B space").count() == 0
+    assert page.get_by_role("heading", name="C space").count() == 0
 
-    response = navigation_info.value
-    assert response.status == 200
+    # click into A space
+    page.get_by_role("link", name="Q1234567").first.click()
+
+    # make sure all values are correct
+    assert (
+        page.get_by_role("row", name="Reference")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["reference"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="Prefix")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["prefix"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="Name")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["name"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="Dataset")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["dataset"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="Start date")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["start_date"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="End date")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["end_date"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="Entry date")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["entry_date"].lower()
+    )
+    assert (
+        page.get_by_role("row", name="Typology")
+        .get_by_role("cell")
+        .first.inner_text()
+        .lower()
+        == mockEntities[0]["typology"].lower()
+    )
 
 
 # This test is currently failing on the pipeline due to line 51 timing out
