f1baf140006624cb53edceb8d329fb22999011e5
George Goodall <ggoodall@scottlogic.com>
2023-08-22 11:45:02 +0100

    fixed python test mocking

--- tests/unit/routers/test_entity.py
+++ tests/unit/routers/test_entity.py
@@ -344,9 +344,14 @@ def test_get_entity_entity_returned_html(
         "application.routers.entity.get_datasets", return_value=multiple_dataset_models
     )
     mocker.patch(
+        "application.core.filters.get_dataset_query",
+        return_value=ancient_woodland_dataset,
+    )
+    mocker.patch(
         "application.routers.entity.get_dataset_query",
         return_value=ancient_woodland_dataset,
     )
+
     request = MagicMock()
     result = get_entity(request=request, entity="11000000", extension=None)
 
@@ -390,7 +395,7 @@ def test_get_entity_entity_returned_json(
 
 
 def test_get_entity_entity_returned_geojson(
-    mocker, single_entity_model, multiple_dataset_models
+    mocker, single_entity_model, multiple_dataset_models, ancient_woodland_dataset
 ):
     mocker.patch(
         "application.routers.entity.get_entity_query",
