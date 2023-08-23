9fc5b81b514adb3a4ae6432dad0b0222bc9262e5
George Goodall <ggoodall@scottlogic.com>
2023-08-23 13:03:39 +0100

    replaced the breadcrumbs with a back button for all the pages

---
+++ assets/javascripts/back-button.js
@@ -0,0 +1,9 @@
+function goBack(href) {
+    const originLength = document.location.origin.length;
+    const originAndPathLength = document.location.origin.length + document.location.pathname.length;
+    const subPath = document.referrer.substring(originLength, originAndPathLength);
+    if(subPath === href) {
+        window.history.back();
+        return false;
+    }
+}
