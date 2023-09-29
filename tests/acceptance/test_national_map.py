cd490504d38ee174406262210ffc0aaca4308ca9
George Goodall <ggoodall@scottlogic.com>
2023-09-29 16:41:35 +0100

    fixed tests

--- tests/acceptance/test_national_map.py
+++ tests/acceptance/test_national_map.py
@@ -39,14 +39,26 @@ def server_process():
     proc.kill()
 
 
+def wait_for_map_layer(page, layer, attempts=10, check_interval=10):
+    for i in range(attempts):
+        isHidden = page.evaluate(
+            f'() => mapControllers.map.map.getLayer("{layer}").isHidden()'
+        )
+        if isHidden is False:
+            return True
+        page.wait_for_timeout(check_interval)
+    assert False, f"Layer {layer} did not appear on the map"
+
+
 def test_toggle_layers_on_the_national_map_correctly_shows_entity(
     server_process, page, add_base_entities_to_database_yield_reset
 ):
-
+    # as the map xy coords are dependent on the viewport size, we need to set it to make sure the tests are consistent
+    page.set_viewport_size({"width": 800, "height": 600})
     page.goto(
         BASE_URL + "/map/#51.0560081663663,-2.260042873039197,10.722039104961226z"
     )
     page.get_by_label("Conservation area").check()
-    page.wait_for_timeout(1000)  # wait for map to load the conservation area layer
-    page.get_by_role("region", name="Map").click(position={"x": 458, "y": 147})
+    wait_for_map_layer(page, "conservation-area-source-fill-extrusion")
+    page.get_by_label("Map").click(position={"x": 215, "y": 156})
     expect(page.get_by_text("Conservation areaStourton")).to_be_visible()
