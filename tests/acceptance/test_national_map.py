3ea0e7ecafb0f789e6306de4e8f3b60cda2579cf
George Goodall <ggoodall@scottlogic.com>
2023-10-02 16:13:07 +0100

    cicd tests

--- tests/acceptance/test_national_map.py
+++ tests/acceptance/test_national_map.py
@@ -46,6 +46,9 @@ def test_toggle_layers_on_the_national_map_correctly_shows_entity(
         BASE_URL + "/map/#50.88865897214836,-2.260771340418273,11.711391365982688z"
     )
     page.wait_for_selector("canvas.maplibregl-canvas")
+    page.screenshot(path="screenshot.png")
+    page.wait_for_selector("button.dl-map__open-btn")
+    page.wait_for_selector("div.dl-map__side-panel")
     page.wait_for_selector("label.govuk-checkboxes__label")
     # isHiddenBeforeClicking = page.evaluate(
     #     'mapControllers.map.map.getLayer("conservation-area-source-fill-extrusion").isHidden()'
