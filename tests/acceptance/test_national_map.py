b4fd445ab2a0b960924b02c1f2c0800360f02689
George Goodall <ggoodall@scottlogic.com>
2023-10-02 12:29:04 +0100

    changed to just wait for a fixed time, as maplibre layer.isHidden doesn't seem to accurately represent if a layer has loaded to the map

--- tests/acceptance/test_national_map.py
+++ tests/acceptance/test_national_map.py
@@ -39,17 +39,6 @@ def server_process():
     proc.kill()
 
 
-def wait_for_map_layer(page, layer, attempts=10, check_interval=10):
-    for i in range(attempts):
-        isHidden = page.evaluate(
-            f'() => mapControllers.map.map.getLayer("{layer}").isHidden()'
-        )
-        if isHidden is False:
-            return True
-        page.wait_for_timeout(check_interval)
-    assert False, f"Layer {layer} did not appear on the map"
-
-
 def test_toggle_layers_on_the_national_map_correctly_shows_entity(
     server_process, page, add_base_entities_to_database_yield_reset
 ):
@@ -59,8 +48,12 @@ def test_toggle_layers_on_the_national_map_correctly_shows_entity(
         BASE_URL + "/map/#50.88865897214836,-2.260771340418273,11.711391365982688z"
     )
     page.get_by_label("Conservation area").check()
-    wait_for_map_layer(page, "conservation-area-source-fill-extrusion")
+    page.wait_for_timeout(
+        1000
+    )  # can take some time to appear on the map, so wait for a second while the data loads
     # note:
     # even with the viewport set to a fixed size this click doesn't always happen in the same place between browsers
     page.get_by_label("Map").click(position={"x": 329, "y": 300})
-    expect(page.get_by_text("Conservation area")).to_be_visible()
+    expect(
+        page.get_by_test_id("map").get_by_role("list").get_by_text("Conservation area")
+    ).to_be_visible()
