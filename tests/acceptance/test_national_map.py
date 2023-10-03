5f4156cc6994e82e48a41dc88cd8dca2b191d1b1
George Goodall <ggoodall@scottlogic.com>
2023-10-03 11:33:21 +0100

    cicd: skip test if webgl not available

--- tests/acceptance/test_national_map.py
+++ tests/acceptance/test_national_map.py
@@ -38,12 +38,14 @@ def server_process():
 
 
 def test_toggle_layers_on_the_national_map_correctly_shows_entity(
-    server_process, page, context, add_base_entities_to_database_yield_reset
+    server_process,
+    page,
+    context,
+    add_base_entities_to_database_yield_reset,
+    skip_if_not_supportsGL,
 ):
     # as the map xy coords are dependent on the viewport size, we need to set it to make sure the tests are consistent
     page.set_viewport_size({"width": 800, "height": 600})
-
-    page.on("console", lambda msg: print("PAGE LOG:", msg.text))
 
     page.goto(
         BASE_URL + "/map/#50.88865897214836,-2.260771340418273,11.711391365982688z"
