9f4d41f2fc3c3dda8bee947cf2fb0820793534dc
George Goodall <ggoodall@scottlogic.com>
2023-10-09 15:57:09 +0100

    move download data for dataset test into the dataset test file

--- tests/acceptance/test_dataset.py
+++ tests/acceptance/test_dataset.py
@@ -7,4 +7,37 @@ def test_roadmap_loads_ok(server_process, BASE_URL, page):
     )
     assert heading.is_visible()
 
-    # make sure the total datasets is a number, and that the data providers is a number
+
+def test_download_data_for_dataset(
+    server_process, BASE_URL, page, add_base_entities_to_database_yield_reset
+):
+    page.goto(BASE_URL)
+
+    # Navigate to the Brownfield site dataset page.
+    page.get_by_role("link", name="Datasets", exact=True).click()
+    page.get_by_role("link", name="Geography").click()
+    page.get_by_role("link", name="Brownfield site").click()
+
+    # Check that the "CSV" download link is correct.
+    csv_href = page.get_by_role("link", name="CSV", exact=True).first.get_attribute(
+        "href"
+    )
+
+    assert "brownfield-site" in csv_href
+    assert ".csv" in csv_href
+
+    # Check that the "JSON" download link is correct.
+    json_href = page.get_by_role("link", name="JSON", exact=True).first.get_attribute(
+        "href"
+    )
+
+    assert "brownfield-site" in json_href
+    assert ".json" in json_href
+
+    # Check that the "GeoJSON" download link is correct.
+    geojson_href = page.get_by_role(
+        "link", name="GeoJSON", exact=True
+    ).first.get_attribute("href")
+
+    assert "brownfield-site" in geojson_href
+    assert ".geojson" in geojson_href
