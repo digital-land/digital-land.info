5301c4ecbbf9ee028170d32ce24ee9eb004763eb
George Goodall <ggoodall@scottlogic.com>
2023-10-09 16:04:34 +0100

    move test_entity_search into test_search

--- tests/acceptance/test_entity_search.py
+++
@@ -1,8 +0,0 @@
-def test_blank_search(server_process, BASE_URL, page):
-    page.goto(BASE_URL)
-    page.click("text=Search")
-
-    with page.expect_response("**/entity/**") as response:
-        page.click("button:has-text(' Search ')")
-
-    assert response.value.ok
--- tests/acceptance/test_search.py
+++ tests/acceptance/test_search.py
@@ -6,3 +6,13 @@ def test_roadmap_loads_ok(server_process, BASE_URL, page):
         name="Search for planning and housing data",
     )
     assert heading.is_visible()
+
+
+def test_blank_search(server_process, BASE_URL, page):
+    page.goto(BASE_URL)
+    page.click("text=Search")
+
+    with page.expect_response("**/entity/**") as response:
+        page.click("button:has-text(' Search ')")
+
+    assert response.value.ok
