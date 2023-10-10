39fd2e547797a2db44895325ec020c16c4b763be
George Goodall <ggoodall@scottlogic.com>
2023-10-10 13:58:37 +0100

    run test 20 times to try and cause cicd test to fail

--- tests/acceptance/test_map.py
+++ tests/acceptance/test_map.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_map_page_loads_ok(server_process, BASE_URL, page):
     response = page.goto(BASE_URL + "/map/")
     assert response.ok
@@ -19,14 +22,15 @@ def wait_for_map_layer(page, layer, attempts=10, check_interval=10):
     assert False, f"Layer {layer} did not appear on the map"
 
 
+@pytest.mark.parametrize("execution_number", range(20))
 def test_toggle_layers_on_the_national_map_correctly_shows_entity(
     server_process,
     page,
-    context,
     add_base_entities_to_database_yield_reset,
     skip_if_not_supportsGL,
     test_settings,
     BASE_URL,
+    execution_number,
 ):
     # as the map xy coords are dependent on the viewport size, we need to set it to make sure the tests are consistent
     page.set_viewport_size({"width": 800, "height": 600})
@@ -36,3 +40,16 @@ def test_toggle_layers_on_the_national_map_correctly_shows_entity(
     )
     page.get_by_label("Conservation area").check()
     wait_for_map_layer(page, "conservation-area-source-fill-extrusion")
+
+
+# def test_using_the_map_to_find_an_entity(
+#     server_process,
+#     page,
+#     add_base_entities_to_database_yield_reset,
+#     skip_if_not_supportsGL,
+#     test_settings,
+#     BASE_URL,
+# ):
+#     page.goto(BASE_URL)
+#     page.get_by_role("link", name="Map", exact=True).click()
+#     page.get_by_label("Conservation area").check()
