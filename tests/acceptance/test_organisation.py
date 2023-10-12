7a91a5c217659379df6d76ddef2c2bafefe47970
George Goodall <ggoodall@scottlogic.com>
2023-10-12 11:45:40 +0100

    added test for navigate to organisation from entity

--- tests/acceptance/test_organisation.py
+++ tests/acceptance/test_organisation.py
@@ -1,3 +1,6 @@
+import re
+
+
 def test_organisation_page_loads_ok(server_process, BASE_URL, page):
     response = page.goto(BASE_URL + "/organisation")
     assert response.ok
@@ -6,3 +9,19 @@ def test_organisation_page_loads_ok(server_process, BASE_URL, page):
         name="Organisations",
     )
     assert heading.is_visible()
+
+
+def test_navigate_to_organisation_from_entity(server_process, BASE_URL, page):
+    page.goto(BASE_URL)
+    page.click("text=Search")
+
+    page.locator("dd").filter(has_text=re.compile(r"^DAC$")).get_by_role("link").click()
+
+    with page.expect_navigation() as navigation_info:
+        page.get_by_role("link", name="organisation").click()
+
+    assert navigation_info.value.ok
+    assert BASE_URL + "/organisation" in navigation_info.value.url
+
+    header = page.get_by_role("heading", name="Organisations")
+    assert header.is_visible()
