c46a159dcbe29cc3874ac85e377e014f1aa1d5bb
George Goodall <ggoodall@scottlogic.com>
2023-10-12 16:03:48 +0100

    change the locator

--- tests/acceptance/test_organisation.py
+++ tests/acceptance/test_organisation.py
@@ -1,6 +1,3 @@
-import re
-
-
 def test_organisation_page_loads_ok(server_process, BASE_URL, page):
     response = page.goto(BASE_URL + "/organisation")
     assert response.ok
@@ -15,7 +12,7 @@ def test_navigate_to_organisation_from_entity(server_process, BASE_URL, page):
     page.goto(BASE_URL)
     page.click("text=Search")
 
-    page.locator("dd").filter(has_text=re.compile(r"^DAC$")).get_by_role("link").click()
+    page.get_by_role("link", name="DAC").first.click()
 
     with page.expect_navigation() as navigation_info:
         page.get_by_role("link", name="organisation").click()
