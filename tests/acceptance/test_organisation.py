43501699d8dbac8afa2ef38f217ed9cb5a664999
George Goodall <ggoodall@scottlogic.com>
2023-10-12 17:52:42 +0100

    Test/navigate to organisation page from entity page (#209)
    * added test for navigate to organisation from entity
    
    * updated changelog
    
    * change the locator
    
    * added a screenshot
    
    * fix screenshot
    
    * add base entities to database
    
    * remove screenshot

--- tests/acceptance/test_organisation.py
+++ tests/acceptance/test_organisation.py
@@ -6,3 +6,21 @@ def test_organisation_page_loads_ok(server_process, BASE_URL, page):
         name="Organisations",
     )
     assert heading.is_visible()
+
+
+def test_navigate_to_organisation_from_entity(
+    server_process, add_base_entities_to_database_yield_reset, BASE_URL, page
+):
+    page.goto(BASE_URL)
+    page.click("text=Search")
+
+    page.get_by_role("link", name="DAC").first.click()
+
+    with page.expect_navigation() as navigation_info:
+        page.get_by_role("link", name="organisation").click()
+
+    assert navigation_info.value.ok
+    assert BASE_URL + "/organisation" in navigation_info.value.url
+
+    header = page.get_by_role("heading", name="Organisations")
+    assert header.is_visible()
