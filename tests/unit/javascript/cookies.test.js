e910f8f640b041ad9daec672742a3d4d972a1bde
George Goodall <ggoodall@scottlogic.com>
2023-10-19 14:52:24 +0100

    added empty tests

---
+++ tests/unit/javascript/cookies.test.js
@@ -0,0 +1,53 @@
+import {describe, expect, test, vi, beforeEach} from 'vitest'
+
+describe('cookies.js', () => {
+    test('deleteCookie', () => {
+
+    })
+
+    test('setCookie', () => {
+
+    })
+
+    test('getCookie', () => {
+
+    })
+
+    test('acceptAllCookies', () => {
+
+    })
+
+    test('showCookieBanner', () => {
+
+    })
+
+    test('hideCookieBanner', () => {
+
+    })
+
+    test('showCookieConfirmation', () => {
+
+    })
+
+    test('hideCookieConfirmation', () => {
+
+    })
+
+    test('setTrackingCookies', () => {
+
+    })
+
+    describe('class cookiePrefs', () => {
+        test('get', () => {
+
+        })
+
+        test('save', () => {
+
+        })
+
+        test('invalidateRejectedCookies', () => {
+
+        })
+    })
+})
