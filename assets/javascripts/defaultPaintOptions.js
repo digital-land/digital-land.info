8c63b2f03145a9022afc1203de8e998a90fe88cd
George Goodall <ggoodall@scottlogic.com>
2024-01-19 11:45:31 +0000

    Revert "make default colour more opaque"
    This reverts commit ea2ff82080ebfffa7c0dce389b5b5a5d3a256f80.

--- assets/javascripts/defaultPaintOptions.js
+++ assets/javascripts/defaultPaintOptions.js
@@ -1,6 +1,6 @@
 export const defaultPaintOptions = {
     'fill-color': '#003078',
-    'fill-opacity': 0.2,
+    'fill-opacity': 0.6,
     'weight': 1,
     'circle-radius': [
         "interpolate", ["linear"], ["zoom"],
