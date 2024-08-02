e174a1489ccb03fc2275463fedb5a01acfd56f66
Feba <feba.rajan@levellingup.gov.uk>
2024-08-02 09:41:26 +0100

    Updated acceptance tests with TestClient and db_session.

--- tests/acceptance/parameters/test_exclude_field_parameter.py
+++ tests/acceptance/parameters/test_exclude_field_parameter.py
@@ -1,65 +1,34 @@
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
 from application.db.models import EntityOrm
 
 
 @pytest.mark.parametrize(
-    "exclude, expected_fields",
-    [
-        (
-            ["organisation-entity"],
-            {
-                "entry-date",
-                "start-date",
-                "end-date",
-                "entity",
-                "name",
-                "dataset",
-                "typology",
-                "reference",
-                "prefix",
-                "point",
-            },
-        ),
-        (
-            ["entry-date", "prefix"],
-            {
-                "start-date",
-                "end-date",
-                "entity",
-                "name",
-                "dataset",
-                "typology",
-                "reference",
-                "organisation-entity",
-                "point",
-            },
-        ),
-    ],
+    "exclude",
+    [["organisation-entity"], ["entry-date", "prefix"]],
 )
 def test_entity_search_exclude_field_for_json_response(
-    app_test_data: dict,
-    client: TestClient,
-    app_db_session: Session,
+    client,
+    db_session,
+    test_data: dict,
     exclude: list,
-    expected_fields: set,
 ):
-    params = {"exclude_field": ",".join(exclude)} if exclude else {}
-
-    # Clear existing data
-    app_db_session.query(EntityOrm).delete()
-    app_db_session.commit()
+    # Clear the table to avoid duplicates
+    db_session.query(EntityOrm).delete()
+    db_session.commit()
 
     # Load entities into the database
-    for entity_data in app_test_data["entities"]:
-        app_db_session.add(EntityOrm(**entity_data))
-        app_db_session.commit()
+    for entity_data in test_data["entities"]:
+        db_session.add(EntityOrm(**entity_data))
+    db_session.commit()
 
+    params = {"exclude_field": ",".join(exclude)} if exclude else {}
     response = client.get("/entity.json", params=params)
     response_json = response.json()
     entities = response_json.get("entities", [])
 
+    assert (
+        len(entities) > 0
+    ), "No entities being returned so cannot check that the fields are correct"
     assert isinstance(entities, list), "Expected 'entities' to be a list"
 
     for entity in entities:
@@ -69,52 +38,49 @@ def test_entity_search_exclude_field_for_json_response(
                 field not in entity
             ), f"Field '{field}' should not be in entity: {entity}"
 
-        # Check if expected fields are present
-        for field in expected_fields:
-            assert (
-                field in entity
-            ), f"Expected field '{field}' not found in entity: {entity}"
-
 
 @pytest.mark.parametrize(
     "expected_fields",
     [
         {
-            "entry-date",
-            "start-date",
-            "entity",
-            "dataset",
-            "typology",
-            "reference",
-            "prefix",
-            "organisation-entity",
-            "geometry",
-            "point",
+            "entry-date": "2019-01-07",
+            "start-date": "2019-01-05",
+            "end-date": "2020-01-07",
+            "entity": "106",
+            "name": "A space",
+            "dataset": "greenspace",
+            "typology": "geography",
+            "reference": "Q1234567",
+            "prefix": "greenspace",
+            "geometry": "MultiPolygon (((-0.3386878967285156 53.74426323597749, -0.337904691696167 53.743857158459996, -0.33673524856567383 53.744003093019586, -0.33637046813964844 53.74463124033804, -0.3365743160247803 53.74525937826645, -0.33737897872924805 53.74541799747043, -0.33875226974487305 53.74505000000031, -0.3386878967285156 53.74426323597749)))",  # noqa: E501
+            "point": "POINT (-0.33737897872924805 53.74541799747043)",
         }
     ],
 )
 def test_entity_search_no_exclude_field_for_json_response(
-    app_test_data: dict,
-    client: TestClient,
-    app_db_session: Session,
+    client,
+    db_session,
+    test_data: dict,
     expected_fields: set,
 ):
-    app_db_session.query(EntityOrm).delete()
-    app_db_session.commit()
+    db_session.query(EntityOrm).delete()
+    db_session.commit()
     # Load entities into the database
-    for entity_data in app_test_data["entities"]:
-        app_db_session.add(EntityOrm(**entity_data))
-        app_db_session.commit()
+    for entity_data in test_data["entities"]:
+        db_session.add(EntityOrm(**entity_data))
+        db_session.commit()
 
     response = client.get("/entity.json")
     assert response.status_code == 200
 
     response_json = response.json()
     entities = response_json.get("entities", [])
-    assert isinstance(entities, list), "Expected 'entities' to be a list"
+
     assert (
         len(entities) > 0
     ), "No entities being returned so cannot check that the fields are correct"
+    assert isinstance(entities, list), "Expected 'entities' to be a list"
+
     for entity in entities:
         for field in expected_fields:
             assert (
@@ -122,59 +88,28 @@ def test_entity_search_no_exclude_field_for_json_response(
             ), f"Expected field '{field}' not found in entity: {entity}"
 
 
-@pytest.mark.parametrize(
-    "exclude, expected_properties",
-    [
-        (
-            ["notes"],
-            {
-                "entry-date",
-                "start-date",
-                "end-date",
-                "entity",
-                "name",
-                "dataset",
-                "typology",
-                "reference",
-                "prefix",
-                "organisation-entity",
-            },
-        ),
-        (
-            ["prefix", "notes"],
-            {
-                "entry-date",
-                "start-date",
-                "end-date",
-                "entity",
-                "name",
-                "dataset",
-                "typology",
-                "reference",
-                "organisation-entity",
-            },
-        ),
-    ],
-)
+@pytest.mark.parametrize("exclude", [["notes"], ["geometry", "notes"]])
 def test_entity_search_exclude_field_for_geojson_response(
-    app_test_data: dict,
-    client: TestClient,
-    app_db_session: Session,
-    exclude: list,
-    expected_properties: set,
+    client, db_session, test_data: dict, exclude: list
 ):
-    app_db_session.query(EntityOrm).delete()
-    app_db_session.commit()
+    db_session.query(EntityOrm).delete()
+    db_session.commit()
     # Load entities into the database
-    for feature_properties in app_test_data["entities"]:
-        app_db_session.add(EntityOrm(**feature_properties))
-        app_db_session.commit()
+    for feature_properties in test_data["entities"]:
+        db_session.add(EntityOrm(**feature_properties))
+        db_session.commit()
 
     # Make the request with the exclude parameters
     params = {"exclude": ",".join(exclude)} if exclude else {}
     response = client.get("/entity.geojson", params=params)
     response_json = response.json()
-    entities = response_json.get("entities", [])
+    assert (
+        len(response_json) > 0
+    ), "No entities being returned so cannot check that the fields are correct"
+    assert "type" in response_json
+    assert "features" in response_json
+
+    entities = response_json["features"]
     assert isinstance(entities, list), "Expected 'features' to be a list"
 
     # Check each feature
@@ -184,48 +119,42 @@ def test_entity_search_exclude_field_for_geojson_response(
             assert (
                 field not in properties
             ), f"Property '{field}' should not be in feature properties: {properties}"
-        for field in expected_properties:
-            assert (
-                field in properties
-            ), f"Expected property '{field}' not found in feature properties: {properties}"
 
 
 @pytest.mark.parametrize(
     "expected_properties",
     [
-        (
-            {
-                "entry-date",
-                "start-date",
-                "end-date",
-                "entity",
-                "name",
-                "dataset",
-                "typology",
-                "reference",
-                "prefix",
-                "organisation-entity",
-                "notes",
-            },
-        )
+        {
+            "entry-date": "2019-01-07",
+            "start-date": "2019-01-05",
+            "end-date": "2020-01-07",
+            "entity": "106",
+            "name": "A space",
+            "dataset": "greenspace",
+            "typology": "geography",
+            "reference": "Q1234567",
+            "prefix": "greenspace",
+        },
     ],
 )
 def test_entity_search_no_exclude_field_for_geojson_response(
-    app_test_data: dict,
-    client: TestClient,
-    app_db_session: Session,
+    client,
+    db_session,
+    test_data: dict,
     expected_properties: set,
 ):
-    app_db_session.query(EntityOrm).delete()
-    app_db_session.commit()
+    db_session.query(EntityOrm).delete()
+    db_session.commit()
     # Load entities into the database
-    for feature_properties in app_test_data["entities"]:
-        app_db_session.add(EntityOrm(**feature_properties))
-        app_db_session.commit()
+    for feature_properties in test_data["entities"]:
+        db_session.add(EntityOrm(**feature_properties))
+        db_session.commit()
 
     response = client.get("/entity.geojson")
     response_json = response.json()
-    entities = response_json.get("entities", [])
+    assert len(response_json) > 0
+
+    entities = response_json["features"]
     assert isinstance(entities, list), "Expected 'features' to be a list"
 
     # Check each feature
