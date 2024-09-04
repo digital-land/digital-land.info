cc173cf4782f7756f0dac58cffd653f4d400d7d6
Chris Cundill <chrisc@diligentsoft.co>
2024-09-04 20:28:34 +0100

    Added return value to canary test

--- tests/smoke/src/smoke_canary.py
+++ tests/smoke/src/smoke_canary.py
@@ -1,7 +1,11 @@
+import asyncio
 import os
+import json
+
+from datetime import datetime
 
 from aws_synthetics.selenium import synthetics_webdriver as syn_webdriver
-from aws_synthetics.common import synthetics_logger as logger
+from aws_synthetics.common import synthetics_logger as logger, CanaryStatus
 from faker import Faker
 
 faker = Faker()
@@ -76,7 +80,17 @@ faker = Faker()
 # https: // www.development.digital - land.info / dataset.json
 
 
-def main(context):
+async def main(event, context):
+    canary_result = CanaryStatus.NO_RESULT.value
+    canary_error = None
+    start_time = None
+    reset_time = None
+    setup_time = None
+    launch_time = None
+    await syn_webdriver.before_canary()
+    launch_time = datetime.now()
+    launch_time = (datetime.now() - launch_time).total_seconds() * 1000
+
     logger.info("UUID: " + faker.uuid4())
     base_url = os.environ.get("BASE_URL")
     url = base_url
@@ -86,9 +100,23 @@ def main(context):
 
     response_code = syn_webdriver.get_http_response(url)
     if not response_code or response_code < 200 or response_code > 299:
+        logger.error(
+            "Failed to load page, did not get expected code: " + str(response_code)
+        )
         raise Exception("Failed to load page!")
 
+    end_time = datetime.now()
     logger.info("Canary successfully executed.")
+    return_value = await syn_webdriver.after_canary(
+        canary_result,
+        canary_error,
+        start_time,
+        end_time,
+        reset_time,
+        setup_time,
+        launch_time,
+    )
+    return json.dumps(return_value)
 
 
 def visit_and_screenshot(browser, url):
@@ -100,5 +128,9 @@ def visit_and_screenshot(browser, url):
         raise Exception("Failed to load page!")
 
 
+# def handler(event, context):
+#     return main()
+
+
 def handler(event, context):
-    return main(context)
+    return asyncio.run(main(event, context))
