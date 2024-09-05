7a783fe7bd68d7579b462427c41f71b4f3f1a82e
Chris Cundill <chrisc@diligentsoft.co>
2024-09-05 07:23:49 +0100

    Trying a non-async handler with a return value

--- tests/smoke/src/smoke_canary.py
+++ tests/smoke/src/smoke_canary.py
@@ -1,11 +1,7 @@
-import asyncio
 import os
-import json
-
-from datetime import datetime
 
 from aws_synthetics.selenium import synthetics_webdriver as syn_webdriver
-from aws_synthetics.common import synthetics_logger as logger, CanaryStatus
+from aws_synthetics.common import synthetics_logger as logger
 from faker import Faker
 
 faker = Faker()
@@ -81,16 +77,6 @@ faker = Faker()
 
 
 async def main(event, context):
-    canary_result = CanaryStatus.NO_RESULT.value
-    canary_error = None
-    start_time = None
-    reset_time = None
-    setup_time = None
-    launch_time = None
-    await syn_webdriver.before_canary()
-    launch_time = datetime.now()
-    launch_time = (datetime.now() - launch_time).total_seconds() * 1000
-
     logger.info("UUID: " + faker.uuid4())
     base_url = os.environ.get("BASE_URL")
     url = base_url
@@ -105,18 +91,7 @@ async def main(event, context):
         )
         raise Exception("Failed to load page!")
 
-    end_time = datetime.now()
     logger.info("Canary successfully executed.")
-    return_value = await syn_webdriver.after_canary(
-        canary_result,
-        canary_error,
-        start_time,
-        end_time,
-        reset_time,
-        setup_time,
-        launch_time,
-    )
-    return json.dumps(return_value)
 
 
 def visit_and_screenshot(browser, url):
@@ -128,9 +103,5 @@ def visit_and_screenshot(browser, url):
         raise Exception("Failed to load page!")
 
 
-# def handler(event, context):
-#     return main()
-
-
 def handler(event, context):
-    return asyncio.run(main(event, context))
+    return main(event, context)
