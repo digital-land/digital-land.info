c231a89b87573e0486f28675c8bcae2b65cf7877
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-11-11 12:57:24 +0000

    add extension to curie route (#319)

--- application/routers/curie.py
+++ application/routers/curie.py
@@ -1,10 +1,15 @@
 import logging
 
+from typing import Optional
 from fastapi import APIRouter, HTTPException, Depends
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 
+from application.core.utils import (
+    DigitalLandJSONResponse,
+)
+from application.search.enum import SuffixEntity
 from application.db.models import LookupOrm, EntityOrm
 from application.db.session import get_session
 
@@ -17,6 +22,7 @@ def get_entity_redirect_by_curie(
     prefix: str,
     reference: str,
     session: Session = Depends(get_session),
+    extension: Optional[SuffixEntity] = None,
 ):
     lookup = (
         session.query(LookupOrm.entity.label("entity"))
@@ -32,14 +38,36 @@ def get_entity_redirect_by_curie(
     if lookup is None:
         raise HTTPException(status_code=404)
     else:
-        url = request.url_for("get_entity", entity=lookup.entity)
+        if extension:
+            url = request.url_for(
+                "get_entity", entity=lookup.entity, extension=extension.value
+            )
+            logging.error(extension.value)
+        else:
+            url = request.url_for("get_entity", entity=lookup.entity)
+
         return RedirectResponse(url, status_code=303)
 
+
+router.add_api_route(
+    "/{prefix}/reference/{reference}.{extension}",
+    endpoint=get_entity_redirect_by_curie,
+    response_class=HTMLResponse,
+    include_in_schema=False,
+)
 
 router.add_api_route(
     "/{prefix}/reference/{reference}",
     endpoint=get_entity_redirect_by_curie,
     response_class=HTMLResponse,
+    include_in_schema=False,
+)
+
+
+router.add_api_route(
+    "/{prefix}:{reference}.{extension}",
+    endpoint=get_entity_redirect_by_curie,
+    response_class=DigitalLandJSONResponse,
     include_in_schema=False,
 )
 
---
+++ tests/acceptance/api_contract/__init__.py
@@ -0,0 +1,6 @@
+"""
+Tests in this folder represent the contract we hold
+with users of this api, it is primarily split by
+route or by specific user groups where a full user journey
+is reuired
+"""
