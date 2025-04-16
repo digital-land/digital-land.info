c359747949dcd029a7f554926c830c7b81c35908
Feba <feba.rajan@levellingup.gov.uk>
2025-04-16 13:37:30 +0100

    added load testing with locust

---
+++ locust/load_test.py
@@ -0,0 +1,37 @@
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)  # defines a random delay between requests
+
+    @task
+    def load_home_page(self):
+        self.client.get("/", name="Home page")
+
+    @task
+    def load_health_check(self):
+        self.client.get("/health", name="Health check")
+
+    @task
+    def load_about_page(self):
+        self.client.get("/about")
+
+    @task
+    def load_dataset_page(self):
+        self.client.get("/dataset")
+
+    @task(8)
+    def load_entity_page(self):
+        self.client.get("/entity")
+
+    @task
+    def load_map_page(self):
+        self.client.get("/map")
+
+    @task
+    def load_api_page(self):
+        self.client.get("/docs")
+
+    @task
+    def load_invalid_geometries(self):
+        self.client.get("/invalid-geometries")
