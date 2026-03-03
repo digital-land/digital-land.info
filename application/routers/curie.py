6b0d5e4e8d88cd48ead475bde0642789000f237a
Pedro <pedro.miguel@live.co.uk>
2026-03-03 09:09:23 +0000

    Implement CURIE duplicate verification functionality

--- application/routers/curie.py
+++ application/routers/curie.py
@@ -10,7 +10,10 @@ from application.core.utils import (
     DigitalLandJSONResponse,
 )
 from application.search.enum import SuffixEntity
-from application.db.models import LookupOrm, EntityOrm
+from application.data_access.curie_queries import (
+    get_lookup_by_curie,
+    get_entity_by_curie,
+)
 from application.db.session import get_session
 
 router = APIRouter()
@@ -24,27 +27,27 @@ def get_entity_redirect_by_curie(
     session: Session = Depends(get_session),
     extension: Optional[SuffixEntity] = None,
 ):
-    lookup = (
-        session.query(LookupOrm.entity.label("entity"))
-        .filter(LookupOrm.prefix == prefix, LookupOrm.reference == reference)
-        .first()
-    )
-    if lookup is None:
-        lookup = (
-            session.query(EntityOrm.entity.label("entity"))
-            .filter(EntityOrm.prefix == prefix, EntityOrm.reference == reference)
-            .first()
-        )
-    if lookup is None:
+    lookup = get_lookup_by_curie(session, prefix, reference)
+    if len(lookup) == 0:
+        lookup = get_entity_by_curie(session, prefix, reference)
+
+    if len(lookup) == 0:
         raise HTTPException(status_code=404)
     else:
         if extension:
             url = request.url_for(
-                "get_entity", entity=lookup.entity, extension=extension.value
+                "get_entity", entity=lookup[0], extension=extension.value
             )
+        elif len(lookup) > 1:
+            # If there are more than 1 entities with the same
+            # CURIE (prefix:reference) as a temporary work-around
+            # we redirect the users to the search page
+            url = request.url_for("search_entities")
         else:
-            url = request.url_for("get_entity", entity=lookup.entity)
-
+            # If the entity is unique there should be only 1 ID
+            # being returned from the query, which we can access
+            # at index 0
+            url = request.url_for("get_entity", entity=lookup[0])
         return RedirectResponse(url, status_code=303)
 
 
