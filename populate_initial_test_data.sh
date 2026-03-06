016a6af691df91ee7c68e6bddb62011f1cb406fb
Pedro <pedro.miguel@live.co.uk>
2026-03-06 16:36:14 +0000

    Add organisations data for local development

--- populate_initial_test_data.sh
+++ populate_initial_test_data.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose exec web python -c "
-from application.db.models import DatasetOrm, EntityOrm
+from application.db.models import DatasetOrm, EntityOrm, OrganisationOrm
 from application.db.session import get_session
-from tests.test_data import datasets, entities
+from tests.test_data import datasets, entities, organisations
 
 session = next(get_session())
 for dataset in datasets:
@@ -11,5 +11,7 @@ for dataset in datasets:
     session.add(DatasetOrm(**dataset, themes=themes.split(',')))
 for entity in entities:
     session.add(EntityOrm(**entity))
+for organisation in organisations:
+    session.add(OrganisationOrm(**organisation))
 session.commit()
 "
