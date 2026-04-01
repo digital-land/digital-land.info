c67e5db60879af5705626c2a8e8e020a571d6806
Pedro Miguel <pedro.miguel@live.co.uk>
2026-04-01 15:27:47 +0100

    Map and re-format quality field value to include description

--- tests/unit/core/test_utils.py
+++ tests/unit/core/test_utils.py
@@ -1,4 +1,10 @@
-from application.core.utils import entity_attribute_sort_key, make_pagination_query_str
+import pytest
+
+from application.core.utils import (
+    entity_attribute_sort_key,
+    map_entity_quality_to_description,
+    make_pagination_query_str,
+)
 
 
 def test_entity_attribute_sort_key_only_excepts_string():
@@ -30,3 +36,46 @@ def test_make_pagination_query_str_preserves_repeated_key_params_when_adding_lim
     result = make_pagination_query_str(query_string_several_datasets, limit, offset)
 
     assert expected == result
+
+
+quality_test_data = [
+    (
+        {"quality": "authoritative", "name": "Brownfield site"},
+        "Authoritative: We have some data from the authoritative source",
+    ),
+    (
+        {"quality": "some", "name": "Historical monument"},
+        "Some: We have some data from an alternative source",
+    ),
+    (
+        {"quality": "trustworthy", "name": "Historic England"},
+        "Trustworthy: We have authorititive data linked to material information",
+    ),
+    (
+        {"quality": "usable", "name": "Green space"},
+        "Usable: We have data from the authoritative source",
+    ),
+]
+
+
+@pytest.mark.parametrize("entity_dict, expected", quality_test_data)
+def test_map_entity_quality_to_description_successfully(entity_dict, expected):
+    """Test mapping the quality field value to the right description."""
+    result = map_entity_quality_to_description(entity_dict)
+    assert result["quality"] == expected
+
+
+def test_map_entity_quality_to_description_with_empty_quality():
+    """Test function when the quality value is `None`."""
+    entity_dict = {"quality": None, "name": "Test Entity"}
+    result = map_entity_quality_to_description(entity_dict)
+    assert result["quality"] == "We have no data"
+
+
+def test_map_entity_quality_to_description_with_unknown_quality():
+    """Test mapping fails, falls back gracefully."""
+    entity_dict = {"quality": "special quality", "name": "Test Entity"}
+    result = map_entity_quality_to_description(entity_dict)
+
+    # Should title case the quality even if description not found
+    assert result["quality"] == "Special quality"
