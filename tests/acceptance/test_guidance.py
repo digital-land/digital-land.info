7467350f987d90c4aa434a09da3e01dab4635ab2
jandums <manbir.jandu@communities.gov.uk>
2026-06-19 15:53:41 +0100

    update test

--- tests/acceptance/test_guidance.py
+++ tests/acceptance/test_guidance.py
@@ -71,25 +71,3 @@ def test_guidance_pages_load_ok(server_url, page):
         name="Update your data",
     )
     assert heading.is_visible()
-
-    # The Open Digital Planning community
-    page.get_by_label("Guidance navigation").get_by_role(
-        "link", name="The Open Digital Planning community"
-    ).click()
-    assert response.ok
-    heading = page.get_by_role(
-        "heading",
-        name="The Open Digital Planning community",
-    )
-    assert heading.is_visible()
-
-    # Get help
-    page.get_by_label("Guidance navigation").get_by_role(
-        "link", name="Get help"
-    ).click()
-    assert response.ok
-    heading = page.get_by_role(
-        "heading",
-        name="Get help",
-    )
-    assert heading.is_visible()
