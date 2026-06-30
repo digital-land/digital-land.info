099b9953bbc4d54931be24552483f7e2fdbc41e8
Matt Poole <matt.a.poole@outlook.com>
2026-06-30 17:49:37 +0100

    urllib deprecation method change (#525)

--- application/data_access/datasette_query_helpers.py
+++ application/data_access/datasette_query_helpers.py
@@ -10,7 +10,7 @@ def get_datasette_http():
     specifically to add retries for larger queries
     """
     retry_strategy = Retry(
-        total=3, status_forcelist=[400], method_whitelist=["GET"], backoff_factor=0
+        total=3, status_forcelist=[400], allowed_methods=["GET"], backoff_factor=0
     )
 
     adapter = HTTPAdapter(max_retries=retry_strategy)
